dcd1b1578dd276ec4f42c8e52eb618e1b468fb83
Onur <cndy121@gmail.com>
2026-08-02 07:55:03 +0300

    fix: activate v0.10 compatibility integration

--- koschei/ergonomics_v010.py
+++ koschei/ergonomics_v010.py
@@ -3,14 +3,18 @@ from .ergonomics_nodes import install_node_references
 from .ergonomics_syntax import install_syntax_v010
 from .ergonomics_semantics import install_semantics_v010
 from .ergonomics_runtime import install_runtime_v010
+from .ergonomics_integration import install_integration_v010
 
 _INSTALLED = False
 
+
 def install_ergonomics_v010() -> None:
     global _INSTALLED
-    if _INSTALLED: return
+    if _INSTALLED:
+        return
     install_node_references()
     install_syntax_v010()
     install_semantics_v010()
     install_runtime_v010()
+    install_integration_v010()
     _INSTALLED = True
