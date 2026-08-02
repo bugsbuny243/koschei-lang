26e43c427e83228b7ed696640d2942f62fe08789
Onur <cndy121@gmail.com>
2026-08-02 08:08:24 +0300

    fix: activate localized ergonomics details

--- koschei/ergonomics_v010.py
+++ koschei/ergonomics_v010.py
@@ -4,6 +4,7 @@ from .ergonomics_syntax import install_syntax_v010
 from .ergonomics_semantics import install_semantics_v010
 from .ergonomics_runtime import install_runtime_v010
 from .ergonomics_integration import install_integration_v010
+from .ergonomics_diagnostics import install_diagnostics_v010
 
 _INSTALLED = False
 
@@ -17,4 +18,5 @@ def install_ergonomics_v010() -> None:
     install_semantics_v010()
     install_runtime_v010()
     install_integration_v010()
+    install_diagnostics_v010()
     _INSTALLED = True
