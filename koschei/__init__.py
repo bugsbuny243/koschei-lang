6b797bd587044f6eb32d573fd5162735f2a82db3
Onur <cndy121@gmail.com>
2026-08-15 10:18:35 +0300

    runtime: install fallible direct MIR adapters

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -95,11 +95,21 @@ from .deterministic_parallel_map_v1 import install_deterministic_parallel_map_v1
 
 install_deterministic_parallel_map_v1()
 
+from .deterministic_parallel_map_mir_shape_v1 import (
+    install_deterministic_parallel_map_mir_shape_v1,
+)
+
+install_deterministic_parallel_map_mir_shape_v1()
+
 from .mir_native_module_shadow_guard_v1 import (
     install_mir_native_module_shadow_guard_v1,
 )
 
 install_mir_native_module_shadow_guard_v1()
+
+from .fallible_mir_v1 import install_fallible_mir_v1
+
+install_fallible_mir_v1()
 
 from .typestate_affine_alignment import install_typestate_affine_alignment
 
