0f514b68fe12493a02d38b348f1f2b93d9e7dc68
Onur <cndy121@gmail.com>
2026-08-15 13:37:24 +0300

    security: install native exact-object integrity guard

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -225,6 +225,12 @@ from .persistence_native_go_parent_integrity_v1 import (
 
 install_persistence_native_go_parent_integrity_v1()
 
+from .persistence_native_go_exact_object_integrity_v1 import (
+    install_persistence_native_go_exact_object_integrity_v1,
+)
+
+install_persistence_native_go_exact_object_integrity_v1()
+
 from .persistence_diagnostics_v1 import install_persistence_diagnostics_v1
 
 install_persistence_diagnostics_v1()
