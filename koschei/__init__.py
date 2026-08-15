c2aa1a9bb3deedeb0fd9a8ae0c9949cf445f3e89
Onur <cndy121@gmail.com>
2026-08-15 10:36:41 +0300

    docs: install serve authority diagnostics v1

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -131,6 +131,10 @@ from .serve_authority_v1 import install_serve_authority_v1
 
 install_serve_authority_v1()
 
+from .serve_authority_diagnostics_v1 import install_serve_authority_diagnostics_v1
+
+install_serve_authority_diagnostics_v1()
+
 from .typestate_affine_alignment import install_typestate_affine_alignment
 
 install_typestate_affine_alignment()
