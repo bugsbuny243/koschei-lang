85ee7b20b5c4788d94fd1a5e5a98b3aa7113fbb2
Onur <cndy121@gmail.com>
2026-08-15 10:47:46 +0300

    security: install canonical loopback authority guard

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -131,6 +131,12 @@ from .serve_authority_v1 import install_serve_authority_v1
 
 install_serve_authority_v1()
 
+from .serve_authority_loopback_canonical_v1 import (
+    install_serve_authority_loopback_canonical_v1,
+)
+
+install_serve_authority_loopback_canonical_v1()
+
 from .serve_authority_diagnostics_v1 import install_serve_authority_diagnostics_v1
 
 install_serve_authority_diagnostics_v1()
