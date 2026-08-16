bcff320c36a4dda8bb54cfbe7337702b24dc1583
Onur <cndy121@gmail.com>
2026-08-16 05:02:23 +0300

    security: install Object Space adversarial guard v1

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -95,6 +95,12 @@ from .deterministic_parallel_map_v1 import install_deterministic_parallel_map_v1
 
 install_deterministic_parallel_map_v1()
 
+from .object_space_adversarial_guard_v1 import (
+    install_object_space_adversarial_guard_v1,
+)
+
+install_object_space_adversarial_guard_v1()
+
 from .typestate_affine_alignment import install_typestate_affine_alignment
 
 install_typestate_affine_alignment()
