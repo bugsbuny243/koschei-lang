c66dacd64fbaee3a43b42512275d9957f7e7847f
Onur <cndy121@gmail.com>
2026-08-16 06:58:42 +0300

    compiler: install native relationship dispatch

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -145,6 +145,10 @@ from .object_space_frontend_alignment_v1 import (
 
 install_object_space_frontend_alignment_v1()
 
+from .native_relationship_alignment_v1 import install_native_relationship_alignment_v1
+
+install_native_relationship_alignment_v1()
+
 from .typestate_affine_alignment import install_typestate_affine_alignment
 
 install_typestate_affine_alignment()
