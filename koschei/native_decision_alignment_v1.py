3707aac973b060deeab30f66443fbf18571bae10
Onur <cndy121@gmail.com>
2026-08-16 07:42:49 +0300

    language: install strict decision vocabulary reservation

--- koschei/native_decision_alignment_v1.py
+++ koschei/native_decision_alignment_v1.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from . import object_space_check_v1 as _check
 from . import object_space_commands_v1 as _commands
+from .native_decision_reservation_v1 import install_native_decision_reservation_v1
 from .native_value_domain_alignment_v1 import check_object_space_graph_with_native_value_domains
 from .object_space_decision_realities_v1 import (
     check_native_decision_object_space,
@@ -24,6 +25,7 @@ def install_native_decision_alignment_v1() -> None:
     global _INSTALLED
     if _INSTALLED:
         return
+    install_native_decision_reservation_v1()
     _check.check_object_space_graph = check_object_space_graph_with_native_decisions
     _commands.check_object_space_graph = check_object_space_graph_with_native_decisions
     _INSTALLED = True
