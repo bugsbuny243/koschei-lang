bb2cdcf3b5400e0c0935c8c178e0c72d5295a2fb
Onur <cndy121@gmail.com>
2026-08-16 11:43:01 +0300

    language: install native cell/reuse composition alignment

--- koschei/__init__.py
+++ koschei/__init__.py
@@ -171,6 +171,12 @@ from .native_cell_projection_alignment_v1 import (
 
 install_native_cell_projection_alignment_v1()
 
+from .native_cell_reuse_composition_alignment_v1 import (
+    install_native_cell_reuse_composition_alignment_v1,
+)
+
+install_native_cell_reuse_composition_alignment_v1()
+
 from .typestate_affine_alignment import install_typestate_affine_alignment
 
 install_typestate_affine_alignment()
